7302c7f3abd7b6153fe1721958477db5d76fb650
EliseChia <swedesurrender@gmail.com>
2019-07-30 20:12:46 +0800

    first commit. have written the tables for SQL.

---
+++ SQL/tables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	first_name TEXT,
+	last_name TEXT,
+	email TEXT,
+	password TEXT
+);
+
+CREATE TABLE IF NOT EXISTS entries (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER,
+	content TEXT,
+	mood_id INTEGER,
+	reason_id INTEGER,
+	created_at DATE DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS podcasts (
+	id SERIAL PRIMARY KEY,
+	mood_id INTEGER,
+	reason_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+	id SERIAL PRIMARY KEY,
+	content TEXT,
+	mood_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS moods (
+	id SERIAL PRIMARY KEY,
+	mood TEXT
+);
+
+CREATE TABLE IF NOT EXISTS reasons (
+	id SERIAL PRIMARY KEY,
+	reasons TEXT
+);
+
+CREATE TABLE IF NOT EXISTS favorites (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER,
+	podcast_id INTEGER
+);
---
+++ db.js
@@ -0,0 +1,63 @@
+/*
+ * ===================================================
+ * ======             CONFIGURATION          =========
+ * ===================================================
+ */
+
+const pg = require('pg');
+const url = require('url');
+var configs;
+
+if( process.env.DATABASE_URL ){
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+} else {
+  configs = {
+    user: 'elisu',
+    host: '127.0.0.1',
+    database: 'minute',
+    port: 5432
+  };
+}
+
+const pool = new pg.Pool(configs);
+
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
+});
+
+/*
+ * ===================================================
+ * ======        REQUIRE MODEL FILES         =========
+ * ===================================================
+ */
+const register = require('./models/register');
+const registerObj = register(pool);
+
+const login = require('./models/login');
+const loginObj = login(pool);
+/*
+ * ===================================================
+ * ======          MODULE EXPORTS            =========
+ * ===================================================
+ */
+
+module.exports = {
+    pool:pool,
+
+    //add all app models below
+    register: registerObj,
+    login: loginObj,
+
+    //each key is now representing an object (based on the model) and we can call the functions written in that object using key.method.
+};

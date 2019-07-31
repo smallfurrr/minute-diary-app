2e1b43ea014aa8c62a7a0c8cc34334735ccf7e29
EliseChia <swedesurrender@gmail.com>
2019-07-31 15:35:26 +0800

    reg form is being a lil b

--- views/pages/register.jsx
+++ views/pages/register.jsx
@@ -6,8 +6,18 @@ class Register extends React.Component {
     return (
           <Layout>
           <div className="reg-header">
-          <h1>minute.</h1>
-          <h2>Registration</h2>
+              <h1>minute.</h1>
+              <h2>Registration</h2>
+          </div>
+
+          <div className="reg-form">
+              <form method="POST" action="/users" id="register-form">
+              <input type="text" name="first_name" className="reg-name" placeholder="First Name"/>
+              <input type="text" name="last_name" className="reg-name" placeholder="Last Name"/>
+              <input type="text" name="email" placeholder="Email"/>
+              <input type="password" name="password" placeholder="Password"/>
+              <button type="submit">Register</button>
+          </form>
           </div>
           </Layout>
         )

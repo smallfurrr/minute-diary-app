8917457d9de19b2d34e7ce8bb1dd765daade7084
EliseChia <swedesurrender@gmail.com>
2019-07-31 15:45:35 +0800

    reg form fixed for now

--- views/pages/register.jsx
+++ views/pages/register.jsx
@@ -12,7 +12,7 @@ class Register extends React.Component {
 
           <div className="reg-form">
               <form method="POST" action="/users" id="register-form">
-              <input type="text" name="first_name" className="reg-name" placeholder="First Name"/>
+              <input type="text" name="first_name" id="first_name" className="reg-name" placeholder="First Name"/>
               <input type="text" name="last_name" className="reg-name" placeholder="Last Name"/>
               <input type="text" name="email" placeholder="Email"/>
               <input type="password" name="password" placeholder="Password"/>

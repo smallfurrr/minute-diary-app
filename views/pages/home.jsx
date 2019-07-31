eecbf910e0873dc84517f1be295bb7e70dbd5dab
EliseChia <swedesurrender@gmail.com>
2019-07-31 11:46:33 +0800

    fixed home page css

--- views/pages/home.jsx
+++ views/pages/home.jsx
@@ -5,14 +5,16 @@ class Home extends React.Component {
   render() {
     return (
           <Layout>
-          <div className = "home-left">
-            <h1>minute.</h1>
-            <img src="img/cloud3.png"/>
-            <h2>A Self-Care Diary App For Busy People</h2>
-          </div>
+          <div id="home-wrapper">
+              <div className = "home-left">
+                <h1>minute.</h1>
+                <img src="img/cloud3.png"/>
+                <h2>A Self-Care Diary App For Busy People</h2>
+              </div>
 
-          <div className = "home-right">
-            <h3>Existing Users</h3>
+              <div className = "home-right">
+                <h3>Existing Users</h3>
+              </div>
           </div>
           </Layout>
         )

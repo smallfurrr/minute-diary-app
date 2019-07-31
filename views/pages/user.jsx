0d3be8b2726dcc810bc53596b49f263624acb7ba
EliseChia <swedesurrender@gmail.com>
2019-07-31 20:40:36 +0800

    starting on mood radio buttons

--- views/pages/user.jsx
+++ views/pages/user.jsx
@@ -6,11 +6,14 @@ class User extends React.Component {
             <LayoutNav cookies={this.props.cookies}>
             <div className="user-wrapper">
                 <h1>Hello {this.props.name}</h1>
-
                 <h2>How are you feeling today?</h2>
-
                 <form method="POST" action="/user" id="entry-form">
-                    <textarea name="content" rows="10" cols="30" maxlength="500" placeholder="Let it out.."></textarea>
+                    <textarea name="content" rows="5" cols="30" maxlength="500" placeholder="Let it out.."></textarea>
+
+                <div id="mood-selection">
+                    <label for="happy"><img src="img/happy.png" alt="happy"/><br/>Happy</label><br/>
+                    <input type="radio" name="mood" value="1" id="happy" />
+                </div>
                     <button type="submit">Post</button>
                 </form>
             </div>

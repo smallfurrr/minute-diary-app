a3db2f2d0aa9614a2ea83ef16079169373668771
EliseChia <swedesurrender@gmail.com>
2019-07-31 19:58:21 +0800

    entry page input area got stylez

--- views/pages/user.jsx
+++ views/pages/user.jsx
@@ -1,14 +1,20 @@
 var React = require("react");
 var LayoutNav = require('../layouts/layout-nav.jsx');
-
 class User extends React.Component {
-  render() {
-    return (
-          <LayoutNav cookies={this.props.cookies}>
-          <div className="user-wrapper">
-          <h1 className="user-greeting">Hello {this.props.name}</h1>
-          </div>
-          </LayoutNav>
+    render() {
+        return (
+            <LayoutNav cookies={this.props.cookies}>
+            <div className="user-wrapper">
+                <h1>Hello {this.props.name}</h1>
+
+                <h2>How are you feeling today?</h2>
+
+                <form method="POST" action="/user" id="entry-form">
+                    <textarea name="content" rows="10" cols="30" maxlength="500" placeholder="Let it out.."></textarea>
+                    <button type="submit">Post</button>
+                </form>
+            </div>
+            </LayoutNav>
         )
     }
 }
